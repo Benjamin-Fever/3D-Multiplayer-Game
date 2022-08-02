d9febc0b8d35402955381c0e555e8af4e6fd27dd
Benjamin Fever <ben.fever07@gmail.com>
2022-08-03 00:22:19 +1200

    Init multiplayer setup

---
+++ Assets/Scripts/NetworkButtons.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine;
+
+public class NetworkButtons : MonoBehaviour
+{
+
+    string ip = "127.0.0.1";
+    string strPort = "7777";
+    private short port;
+    
+    private void Awake(){
+        string[] args = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-launch-as-server")
+            {
+                NetworkManager.Singleton.StartServer();
+            }
+        }
+    }
+
+    private void OnGUI() {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if (!NetworkManager.Singleton.IsClient)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            if (GUILayout.Button("Connect To Server")) NetworkManager.Singleton.StartClient();
+            ip = GUILayout.TextField(ip, 15);
+
+            strPort = GUILayout.TextField(strPort, 5);
+            if (short.TryParse(strPort, out port))
+            {
+                GetComponent<UnityTransport>().SetConnectionData(ip, (ushort)port);
+            }
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        GUILayout.EndArea();
+    }
+
+    // private void Awake() {
+    //     GetComponent<UnityTransport>().SetDebugSimulatorParameters(
+    //         packetDelay: 120,
+    //         packetJitter: 5,
+    //         dropRate: 3);
+    // }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.Netcode;
+using UnityEngine;
+
+
+
+public class PlayerController : NetworkBehaviour
+{
+    public float speed = 12f;
+    public float sensitivty = 100f;
+    
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+
+    void Start()
+    {
+        Camera.main.transform.position = transform.position + Vector3.up;
+        Camera.main.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+        
+        var mouseX = Input.GetAxis("Mouse X") * sensitivty * Time.deltaTime;
+        var mouseY = Input.GetAxis("Mouse Y") * sensitivty * Time.deltaTime;
+
+        var move = transform.right * x + transform.forward * z;
+        GetComponent<CharacterController>().Move(move * speed * Time.deltaTime);
+        
+        xRotation -= mouseY;
+        yRotation += mouseX;
+        
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        Camera.main.transform.position = transform.position + Vector3.up;
+
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) Destroy(this);
+    }
+}
