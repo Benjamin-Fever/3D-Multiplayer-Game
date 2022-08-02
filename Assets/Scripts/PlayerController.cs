63721ca4ae8b51065c8ddcb8a95fe9ece0d0fd41
Benjamin Fever <ben.fever07@gmail.com>
2022-08-03 09:27:08 +1200

    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,24 +26,22 @@ public class PlayerController : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        var x = Input.GetAxis("Horizontal");
-        var z = Input.GetAxis("Vertical");
-        
         var mouseX = Input.GetAxis("Mouse X") * sensitivty * Time.deltaTime;
         var mouseY = Input.GetAxis("Mouse Y") * sensitivty * Time.deltaTime;
-
-        var move = transform.right * x + transform.forward * z;
-        GetComponent<CharacterController>().Move(move * speed * Time.deltaTime);
         
         xRotation -= mouseY;
         yRotation += mouseX;
-        
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         
         Camera.main.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
         Camera.main.transform.position = transform.position + Vector3.up;
-
         transform.Rotate(Vector3.up * mouseX);
+        
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+        
+        var move = transform.right * x + transform.forward * z;
+        GetComponent<CharacterController>().Move(move * speed * Time.deltaTime);
     }
 
     public override void OnNetworkSpawn()
